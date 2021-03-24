daaf2fa55d70f704675b1e6e12caeb2d047328ee
Andreas Zweili <andreas@2li.ch>
2021-03-24 23:10:25 +0100

    remove the makemigrations from run.shell
    I'm going to checking them in from now on so no need to build them each time.

--- run.sh
+++ run.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 sleep 2
-python manage.py makemigrations backups
-python manage.py makemigrations computers
-python manage.py makemigrations core
-python manage.py makemigrations customers
-python manage.py makemigrations devices
-python manage.py makemigrations licenses
-python manage.py makemigrations nets
-python manage.py makemigrations softwares
-python manage.py makemigrations users
-python manage.py makemigrations
 python manage.py migrate
 
 if $(python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')"); then
