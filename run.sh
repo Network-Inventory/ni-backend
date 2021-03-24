3faf72d6de1b9436de1994ff7b29d724dd54e375
Andreas Zweili <andreas@2li.ch>
2021-03-24 19:13:55 +0100

    rewrite the run script
    now it only loads the initial data when the default admin isn't present.
    This can obviously fail if someone deletes the default admin but for the
    moment it's better than what we had before.

--- run.sh
+++ run.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
-if [ -f /var/cache/network_inventory/.second_run ]; then
-    sleep 2
-    python manage.py makemigrations
-    python manage.py migrate
-else
-    python manage.py makemigrations backups
-    python manage.py makemigrations computers
-    python manage.py makemigrations core
-    python manage.py makemigrations customers
-    python manage.py makemigrations devices
-    python manage.py makemigrations licenses
-    python manage.py makemigrations nets
-    python manage.py makemigrations softwares
-    python manage.py makemigrations users
-    python manage.py makemigrations
-    python manage.py migrate
+sleep 2
+python manage.py makemigrations backups
+python manage.py makemigrations computers
+python manage.py makemigrations core
+python manage.py makemigrations customers
+python manage.py makemigrations devices
+python manage.py makemigrations licenses
+python manage.py makemigrations nets
+python manage.py makemigrations softwares
+python manage.py makemigrations users
+python manage.py makemigrations
+python manage.py migrate
+
+if $(python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')"); then
     python manage.py loaddata backups
     python manage.py loaddata computers
     python manage.py loaddata core
     python manage.py loaddata devices
     python manage.py loaddata nets
     python manage.py loaddata softwares
-    python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@example.com', 'password')"
-    mkdir -p /var/cache/network_inventory
-    touch /var/cache/network_inventory/.second_run
 fi
 find . \( -name __pycache__ -o -name "*.pyc" \) -delete
 gunicorn network_inventory.wsgi:application --reload --bind 0.0.0.0:8000 --workers 3
